2c36a1aa5d78ad77bc48e8707effc1ca1b314690
christian <arthurchristian111@gmail.com>
2023-01-13 09:20:49 +0000

    added error messages if login fails

--- src/app/component/login-form/login-form.component.ts
+++ src/app/component/login-form/login-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {  FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 import { PostLogin } from 'src/app/post.model';
 import { PostsService } from 'src/service/post.service';
 import { Router } from '@angular/router';
@@ -7,35 +7,44 @@ import { map } from 'rxjs';
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',
-  styleUrls: ['./login-form.component.css']
+  styleUrls: ['./login-form.component.css'],
 })
-
 export class LoginFormComponent implements OnInit {
   signUp!: FormGroup;
   submitted: boolean = false;
   isFetching: boolean = false;
   show = true;
   password: any;
+  user: boolean = false;
 
-
-  constructor(private formBuilder: FormBuilder, private postsService:PostsService, private route: Router) { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private postsService: PostsService,
+    private route: Router
+  ) {}
 
   buildLoginForm(): void {
     this.signUp = this.formBuilder.group({
-      email: ['', [Validators.required, Validators.email, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$")]],
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.email,
+          Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$'),
+        ],
+      ],
       password: ['', [Validators.required, Validators.minLength(8)]],
-    })
+    });
   }
 
   ngOnInit(): void {
     this.signUp;
     this.buildLoginForm();
     this.onGetPosts;
-    this.password = 'password'
+    this.password = 'password';
   }
 
-
- //toggle hide and show password
+  //toggle hide and show password
   onClick() {
     if (this.password === 'password') {
       this.password = 'text';
@@ -46,48 +55,48 @@ export class LoginFormComponent implements OnInit {
     }
   }
 
-
- //GET post
- onGetPosts(){
-    this.isFetching = true
-    this.postsService.getPosts().pipe(
-      map(ResponseData =>{
-        const postArray = []
-        for(const key in ResponseData){
-          if(ResponseData.hasOwnProperty(key)){
-            postArray.push({...ResponseData})
-          }
+  //GET post
+  onGetPosts() {
+    this.isFetching = true;
+    this.postsService.getPosts().subscribe({
+      next: (responseData: any) => {
+        this.isFetching = false;
+        const userArray = [];
+        for (const key in responseData) {
+          console.log(responseData[key]);
+          userArray.push(responseData[key]);
         }
-        return postArray
-      })
-    ).subscribe(responseData => {
-    this.isFetching = false
-    console.log(responseData);
-    const user = responseData.find((a:any) => {
-      return a.email === this.signUp.value.email && a.password === this.signUp.value.password;
+        userArray.find((res: any) => {
+          this.user =
+            res.email === this.signUp.value.email &&
+            res.password === this.signUp.value.password;
+          console.log(this.user);
+
+          if (this.user) {
+            this.route.navigate(['news']);
+
+            alert('Login Successfully');
+          } else {
+            alert('user not found');
+          }
+        });
+      }
+      // ,
+      // error: (err: any) => {
+      //   alert(err);
+      // },
     });
-    console.log(user)
-    if(Object.keys(user).length){
-      alert('Login Successfully')
-      // this.signUp.reset()
-      this.route.navigate(['news'])
-     } else{
-      alert('user not found')
-    }
-    })
   }
 
   onSubmit(postData: PostLogin) {
     this.submitted = true;
 
     if (this.signUp.invalid) {
-      return
-    }else{
+      return;
+    } else {
       this.onGetPosts();
     }
     // this.signUp.reset();
-    this.submitted = false
-
+    this.submitted = false;
   }
-
 }
