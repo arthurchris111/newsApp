8aec276075bd6926192debb6de23d32a619a8b6c
christian <arthurchristian111@gmail.com>
2023-01-24 22:03:56 +0000

    registration toggle password

--- src/app/component/registration-form/registration-form.component.ts
+++ src/app/component/registration-form/registration-form.component.ts
@@ -10,37 +10,52 @@ import { PostsService } from 'src/service/post.service';
 @Component({
   selector: 'app-registration-form',
   templateUrl: './registration-form.component.html',
-  styleUrls: ['./registration-form.component.css']
+  styleUrls: ['./registration-form.component.css'],
 })
 export class RegistrationFormComponent implements OnInit {
   registration!: FormGroup;
   submitted: boolean = false;
+  show: boolean = false;
   // service: any;
-  user: any = {}
+  user: any = {};
   userSubmitted: boolean = false;
 
-  constructor(private FormBuilder: FormBuilder, private http: HttpClient, private PostsService: PostsService, private route: Router) { }
+  constructor(
+    private FormBuilder: FormBuilder,
+    private http: HttpClient,
+    private PostsService: PostsService,
+    private route: Router
+  ) {}
 
   buildRegistrationForm(): void {
     this.registration = this.FormBuilder.group({
       name: ['', Validators.required],
       surname: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$")]],
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.email,
+          Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$'),
+        ],
+      ],
       password: ['', [Validators.required, Validators.minLength(8)]],
-    })
+    });
   }
 
   ngOnInit(): void {
     this.registration;
     this.buildRegistrationForm();
     this.PostsService.getPosts();
-
-
   }
 
   // onCreateAndStorePost(postData:postRegistration){
   //   this.PostsService.createAndStorePost(postData.name,postData.surname,postData.email,postData.password)
   // }
+
+  passwordToggle() {
+    this.show = !this.show;
+  }
 
   onSubmit(postData: postRegistration) {
     this.submitted = true;
@@ -48,22 +63,25 @@ export class RegistrationFormComponent implements OnInit {
 
     //route registration page to login if valid
     if (this.registration.invalid) {
-      return
+      return;
     } else {
-      this.route.navigate(['login'])
+      this.route.navigate(['login']);
     }
 
     //saving user details to local storage
-    this.user = Object.assign(this.user, this.registration.value)
-    this.PostsService.addUser(this.user)
-    console.log(this.registration.value)
+    this.user = Object.assign(this.user, this.registration.value);
+    this.PostsService.addUser(this.user);
+    console.log(this.registration.value);
 
     this.registration.reset();
-    this.submitted = false
+    this.submitted = false;
     this.userSubmitted = false;
 
-    this.PostsService.createAndStorePost(postData.name, postData.surname, postData.email, postData.password)
+    this.PostsService.createAndStorePost(
+      postData.name,
+      postData.surname,
+      postData.email,
+      postData.password
+    );
   }
-
-
 }
